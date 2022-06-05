f396b5cb90a3bd521ffac16431c47ec17e1f8666
Indan-Ghosh <Koolindan@gmail.com>
2022-06-05 09:44:10 +0000

    full code

--- gitscript.sh
+++ gitscript.sh
@@ -2,4 +2,6 @@ git add .
 echo -n "what is this change for?"
 read;
 git commit -m "${REPLY}"
-git push
+git remote add origin https://github.com/Indan-Ghosh/CAPM_FINAL.git
+git branch -M main
+git push -u origin main
