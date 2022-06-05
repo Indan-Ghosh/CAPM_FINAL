219dc13d109b178a1f6d4bc5269aba95fcae6957
Indan-Ghosh <Koolindan@gmail.com>
2022-06-05 10:05:52 +0000

    checking push in Git

--- gitscript.sh
+++ gitscript.sh
@@ -2,6 +2,6 @@ git add .
 echo -n "what is this change for?"
 read;
 git commit -m "${REPLY}"
-git remote add origin https://github.com/Indan-Ghosh/CAPM_FINAL.git
+git remote add origin https://github.com/Indan-Ghosh/checkpush.git
 git branch -M main
 git push -u origin main
